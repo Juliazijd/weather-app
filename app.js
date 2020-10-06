ddc8b3c01acb6fe3f4bf29d3db4119d8dba38661
Juliazijd <68395721+Juliazijd@users.noreply.github.com>
2020-10-06 12:49:21 +0200

    Display last updated time

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-//function getTime(timestamp) {
-//    let currentTime = new Date(timestamp);
-//    let hours = currentTime.getHours();
-//    if (hours < 10) {
-//        hours = `0${hours}`;
-//        let minutes = currentTime.minutes();
-//        if (minutes < 10) {
-//            minutes = `0${hours}`;
-//            let day = currentTime.getDay();
-//
-//            return `- ${day} ${hours}:${minutes}`;
-//        }
+function getTime(timestamp) {
+  let currentTime = new Date(timestamp);
+  let hours = currentTime.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentTime.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
 function displayWeather(response) {
   let city = document.querySelector("#city");
