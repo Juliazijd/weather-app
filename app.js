abc7daf0d3471c500230268704fd05964d544fe4
Juliazijd <68395721+Juliazijd@users.noreply.github.com>
2020-10-06 17:00:32 +0200

    Added weather forecast

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 function getTime(timestamp) {
   let currentTime = new Date(timestamp);
-  let hours = currentTime.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = currentTime.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${hours}`;
-  }
+
   let days = [
     "Sunday",
     "Monday",
@@ -18,7 +11,20 @@ function getTime(timestamp) {
     "Saturday",
   ];
   let day = days[currentTime.getDay()];
-  return `${day} ${hours}:${minutes}`;
+  return `${day} ${displayForecastHours(timestamp)}`;
+}
+
+function displayForecastHours(timestamp) {
+  let currentTime = new Date(timestamp);
+  let hours = currentTime.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
 }
 
 function displayWeather(response) {
@@ -45,10 +51,143 @@ function displayWeather(response) {
   weatherIcon.setAttribute("alt", response.data.weather[0].description);
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.list[0];
+  forecastElement.innerHTML = `
+                <div class="col-2">
+                  <h4 id="forecast-hours">${displayForecastHours(
+                    forecast.dt * 1000
+                  )}</h4>
+                  <img
+                    src="http://openweathermap.org/img/wn/${
+                      forecast.weather[0].icon
+                    }@2x.png"
+                    alt="${forecast.weather[0].description}"
+                    class="forecast-icon"
+                  />
+                  <strong id="forecast-temp-max">${Math.round(
+                    forecast.main.temp_max
+                  )}</strong>|${Math.round(forecast.main.temp_min)}°C
+                   <div id="forecast-windspeed"> ${Math.round(
+                     forecast.wind.speed * 3.29084
+                   )}km/h</div>
+                </div>`;
+
+  forecast = response.data.list[1];
+  forecastElement.innerHTML += `
+                  <div class="col-2">
+                  <h4 id="forecast-hours">${displayForecastHours(
+                    forecast.dt * 1000
+                  )}</h4>
+                  <img
+                    src="http://openweathermap.org/img/wn/${
+                      forecast.weather[0].icon
+                    }@2x.png"
+                    alt="${forecast.weather[0].description}"
+                    class="forecast-icon"
+                  />
+                  <strong id="forecast-temp-max">${Math.round(
+                    forecast.main.temp_max
+                  )}</strong>|${Math.round(forecast.main.temp_min)}°C
+                   <div id="forecast-windspeed"> ${Math.round(
+                     forecast.wind.speed * 3.29084
+                   )}km/h</div>
+                </div>`;
+
+  forecast = response.data.list[2];
+  forecastElement.innerHTML += `
+                  <div class="col-2">
+                  <h4 id="forecast-hours">${displayForecastHours(
+                    forecast.dt * 1000
+                  )}</h4>
+                  <img
+                    src="http://openweathermap.org/img/wn/${
+                      forecast.weather[0].icon
+                    }@2x.png"
+                    alt="${forecast.weather[0].description}"
+                    class="forecast-icon"
+                  />
+                  <strong id="forecast-temp-max">${Math.round(
+                    forecast.main.temp_max
+                  )}</strong>|${Math.round(forecast.main.temp_min)}°C
+                   <div id="forecast-windspeed"> ${Math.round(
+                     forecast.wind.speed * 3.29084
+                   )}km/h</div>
+                </div>`;
+
+  forecast = response.data.list[3];
+  forecastElement.innerHTML += `
+                  <div class="col-2">
+                  <h4 id="forecast-hours">${displayForecastHours(
+                    forecast.dt * 1000
+                  )}</h4>
+                  <img
+                    src="http://openweathermap.org/img/wn/${
+                      forecast.weather[0].icon
+                    }@2x.png"
+                    alt="${forecast.weather[0].description}"
+                    class="forecast-icon"
+                  />
+                  <strong id="forecast-temp-max">${Math.round(
+                    forecast.main.temp_max
+                  )}</strong>|${Math.round(forecast.main.temp_min)}°C
+                   <div id="forecast-windspeed"> ${Math.round(
+                     forecast.wind.speed * 3.29084
+                   )}km/h</div>
+                </div>`;
+
+  forecast = response.data.list[4];
+  forecastElement.innerHTML += `
+                  <div class="col-2">
+                  <h4 id="forecast-hours">${displayForecastHours(
+                    forecast.dt * 1000
+                  )}</h4>
+                  <img
+                    src="http://openweathermap.org/img/wn/${
+                      forecast.weather[0].icon
+                    }@2x.png"
+                    alt="${forecast.weather[0].description}"
+                    class="forecast-icon"
+                  />
+                  <strong id="forecast-temp-max">${Math.round(
+                    forecast.main.temp_max
+                  )}</strong>|${Math.round(forecast.main.temp_min)}°C
+                   <div id="forecast-windspeed"> ${Math.round(
+                     forecast.wind.speed * 3.29084
+                   )}km/h</div>
+                </div>`;
+
+  forecast = response.data.list[5];
+  forecastElement.innerHTML += `
+                  <div class="col-2">
+                  <h4 id="forecast-hours">${displayForecastHours(
+                    forecast.dt * 1000
+                  )}</h4>
+                  <img
+                    src="http://openweathermap.org/img/wn/${
+                      forecast.weather[0].icon
+                    }@2x.png"
+                    alt="${forecast.weather[0].description}"
+                    class="forecast-icon"
+                  />
+                  <strong id="forecast-temp-max">${Math.round(
+                    forecast.main.temp_max
+                  )}</strong>|${Math.round(forecast.main.temp_min)}°C
+                   <div id="forecast-windspeed"> ${Math.round(
+                     forecast.wind.speed * 3.29084
+                   )}km/h</div>
+                </div>`;
+}
+
 function search(city) {
   let apiKey = "427b64eee1edb35b88796269421b55f1";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
@@ -62,7 +201,7 @@ function handlePosition(position) {
   let longitude = position.coords.longitude;
   let apiKey = "427b64eee1edb35b88796269421b55f1";
   let apiUrlCurrentLocation = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrlCurrentLocation).then(displayWeather);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function getPosition() {
@@ -75,4 +214,4 @@ searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#search-current-location");
 currentLocationButton.addEventListener("click", getPosition);
 
-search("New York");
+search("Amsterdam");
