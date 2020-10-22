663ab7d0971950d1e74e44765d7cc35a7e9a09a2
Juliazijd <68395721+Juliazijd@users.noreply.github.com>
2020-10-22 09:57:05 +0200

    Adjusted folder structure

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import React from "react";
+import SearchForm from "./SearchForm.js"
+
+export default function App() {
+
+}
+
 function getTime(timestamp) {
   let currentTime = new Date(timestamp);
 
